b699f6b2314d6ffd04f558fd33918265200035f9
Sophiagarcia68 <sophia.machado293@gmail.com>
2024-11-24 10:33:04 -0300

    config versioning

--- web/sites/settings.local.php
+++ web/sites/settings.local.php
@@ -149,3 +149,4 @@ $settings['skip_permissions_hardening'] = TRUE;
  * the language or field module.
  */
 # $settings['config_exclude_modules'] = ['devel', 'stage_file_proxy'];
+$settings['config_sync_directory'] = '../config/sync';
